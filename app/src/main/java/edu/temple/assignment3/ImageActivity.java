84c84d458e9fa32e6554ce74c6fcd225fab274f2
tuj55883 <78548481+tuj55883@users.noreply.github.com>
2021-02-23 18:02:09 -0500

    Now in the spinner there is a picture displayed to the left of the text. Final Build

--- app/src/main/java/edu/temple/assignment3/ImageActivity.java
+++ app/src/main/java/edu/temple/assignment3/ImageActivity.java
@@ -14,7 +14,7 @@ import android.widget.TextView;
 
 public class ImageActivity extends AppCompatActivity {
 
-
+    //Initializes the views we will use
     Spinner spinner;
     ImageView image_view;
     int[] bean_res_id;
@@ -25,28 +25,32 @@ public class ImageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        //Sets the views to their ids
         image_view = findViewById(R.id.image_view);
         spinner = findViewById(R.id.spinner);
         textView = findViewById(R.id.textView);
 
-        String[] bean_array = new String[]{"Kidney Bean", "Black Bean", "Green Bean", "Mung Bean", "Pinto Bean"};
-        bean_res_id = new int[]{R.drawable.bean,R.drawable.black_bean,R.drawable.green_bean,R.drawable.mung_bean,R.drawable.pinto};
+        //Makes the array of beans we use and the other array are the images
+        String[] bean_array = new String[]{"Please Pick A Bean","Kidney Bean", "Black Bean", "Green Bean", "Mung Bean", "Pinto Bean"};
+        bean_res_id = new int[]{R.drawable.empty,R.drawable.bean,R.drawable.black_bean,R.drawable.green_bean,R.drawable.mung_bean,R.drawable.pinto};
 
-        //ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, bean_array);
-        ImageAdapter adapter = new ImageAdapter(this, bean_array);
+        //Uses custom image adapter class
+        ImageAdapter adapter = new ImageAdapter(this, bean_array,bean_res_id);
 
-
+        //Sets our spinner to our adapter
         spinner.setAdapter(adapter);
 
 
-
+        //When item is selected it sets the name and picture to said item
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                //showPicture(position);
                 show_image(position);
-                textView.setText(parent.getItemAtPosition(position).toString());
+                if(position>0) {//Makes sure something is selected
+                    textView.setText(parent.getItemAtPosition(position).toString());
+                } else {
+                    textView.setText(" ");
+                }
             }
 
             @Override
@@ -57,6 +61,7 @@ public class ImageActivity extends AppCompatActivity {
 
 
         }
+     //Quick function to set image
     private void show_image(int position) {
         image_view.setImageResource(bean_res_id[position]);
     }
--- app/src/main/java/edu/temple/assignment3/ImageAdapter.java
+++ app/src/main/java/edu/temple/assignment3/ImageAdapter.java
@@ -1,30 +1,36 @@
 package edu.temple.assignment3;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.util.ArrayList;
+import androidx.core.content.ContextCompat;
 
+import java.util.ArrayList;
+//This is the adapter class made that converts the spinner to an editable thing
 public class ImageAdapter extends BaseAdapter {
 
     Context context;
     String[] items;
+    int[] bean_res_id;
 
-    public ImageAdapter(Context context, String[] items){
+    public ImageAdapter(Context context, String[] items, int[] bean_res_id){
         this.context = context;
         this.items = items;
+        this.bean_res_id = bean_res_id;
     }
 
     @Override
-    public int getCount() {
+    public int getCount() { //Gets the amount of items
         return items.length;
     }
 
     @Override
-    public Object getItem(int position) {
+    public Object getItem(int position) { //gets a specific item
         return items[position];
     }
 
@@ -34,6 +40,7 @@ public class ImageAdapter extends BaseAdapter {
     }
 
     @Override
+    //This gets the view when selected
     public View getView(int position, View convertView, ViewGroup parent) {
         TextView textView =  new TextView(context);
         textView.setText(items[position]);
@@ -41,5 +48,27 @@ public class ImageAdapter extends BaseAdapter {
         textView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
         textView.setPadding(10,8,0,8);
         return textView;
+    }
+
+    @Override
+    //This gets the view in the drop down
+    public View getDropDownView(int position, View convertView, ViewGroup parent) {
+        TextView pair = (TextView) super.getDropDownView(position, convertView, parent);
+
+
+        //This sets the image to the left of the text
+        Drawable img =context.getDrawable(bean_res_id[position]);
+        img.setBounds(0, 0, 64, 74);
+        pair.setCompoundDrawables(img, null, null, null);
+
+
+        //Sets text
+        pair.setText(items[position]);
+
+
+        
+
+        
+        return pair;
     }
 }
